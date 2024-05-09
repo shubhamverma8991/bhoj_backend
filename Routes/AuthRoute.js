b7b91d69984827363755dade8c13573e291b98a3
WickTech <rishabhverma707@gmail.com>
2024-05-09 23:40:05 +0530

    ship route

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -1,5 +1,6 @@
 const { Signup, Login ,getAllUsers} = require("../Controllers/AuthController");
 const {userVerification} = require("../Middlewares/Authmiddleware");
+const {createShipment} = require("../Controllers/ShipmentController");
 
 const router = require("express").Router();
 
@@ -7,5 +8,6 @@ router.post("/signup", Signup);
 router.post("/login", Login);
 router.post("/verify",userVerification);
 router.get("/users", getAllUsers);
+router.post("/createShip",createShipment);
 
 module.exports = router;
