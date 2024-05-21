6959c3474276b71d47c73ff95b05dd9edd0d71ae
WickTech <rishabhverma707@gmail.com>
2024-05-21 19:35:33 +0530

    CRUD methods for client table fixed

--- Routes/ClientRoute.js
+++ Routes/ClientRoute.js
@@ -12,9 +12,16 @@ const router = require("express").Router();
 
 router.post("/createClient", createClient);
 router.get("/getAllClients", getAllClients);
+// Route to get a client by clientId
 router.get("/getClientByClientId", getClientByClientId);
+
+// Route to update a client by clientId
 router.put("/updateClientByClientId", updateClientByClientId);
+
+// Route to delete a client by clientId
 router.delete("/deleteClientByClientId", deleteClientByClientId);
-router.get("/searchClientByName", searchClientByName);
+
+// Route to search clients by name
+router.get("/clients/search", searchClientByName);
 
 module.exports = router;
